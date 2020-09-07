88fb93049256676fb4ceed9ac4caf30bcff7f0f2
Depressia <cray86@mail.ru>
2020-09-07 06:43:06 +0300

    Адаптация для 300-600.

---
+++ source/js/menuToggle.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+  var jobList = document.querySelector(".job-list");
+  var navToggle = document.querySelector(".main-nav__toggle");
+
+  jobList.classList.remove("job-list--nojs");
+
+  navToggle.addEventListener("click", function() {
+    if (jobList.classList.contains("job-list--closed")) {
+      jobList.classList.remove("job-list--closed");
+      jobList.classList.add("job-list--opened");
+    } else {
+      jobList.classList.add("job-list--closed");
+      jobList.classList.remove("job-list--opened");
+    }
+  })
+})();
