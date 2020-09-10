461ef30978aff100b66b634e2f959e19530c5e99
Depressia <cray86@mail.ru>
2020-09-10 21:51:32 +0300

    Меняет меню на 660 и меньше.

--- source/js/menuToggle.js
+++ source/js/menuToggle.js
@@ -3,6 +3,7 @@
 (function () {
   var jobList = document.querySelector(".job-list");
   var navToggle = document.querySelector(".main-nav__toggle");
+  var header = document.querySelector(".page-header");
 
   jobList.classList.remove("job-list--nojs");
 
@@ -10,9 +11,11 @@
     if (jobList.classList.contains("job-list--closed")) {
       jobList.classList.remove("job-list--closed");
       jobList.classList.add("job-list--opened");
+      header.classList.add("page-header--blue");
     } else {
       jobList.classList.add("job-list--closed");
       jobList.classList.remove("job-list--opened");
+      header.classList.remove("page-header--blue");
     }
   })
 })();
